126a5ca92e65e6dd7adb4c3210fd3425c3fc232d
Jokwanhee <jonotch1@naver.com>
2023-11-21 21:34:51 +0900

    [UI] : neighborhood life #11
    동네생활 UI 구현 완료

--- app/src/main/java/org/sopt/carrot/presentation/neighborhoodlife/NeighborhoodLifeFragment.kt
+++ app/src/main/java/org/sopt/carrot/presentation/neighborhoodlife/NeighborhoodLifeFragment.kt
@@ -1,22 +1,17 @@
 package org.sopt.carrot.presentation.neighborhoodlife
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import org.sopt.carrot.R
 import org.sopt.carrot.core.ui.base.BindingFragment
 import org.sopt.carrot.databinding.FragmentNeighborhoodLifeBinding
-import org.sopt.carrot.presentation.exploremeeting.ExploreMeetingActivity
 
 class NeighborhoodLifeFragment :
     BindingFragment<FragmentNeighborhoodLifeBinding>(R.layout.fragment_neighborhood_life) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnSample.setOnClickListener {
-            startActivity(Intent(requireContext(), ExploreMeetingActivity::class.java))
-        }
     }
 
     fun newInstance(): Fragment = NeighborhoodLifeFragment()
