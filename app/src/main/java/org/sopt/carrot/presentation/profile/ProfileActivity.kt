3db9d617331b2f31d84aa2fffa0adbd8da29e983
Jokwanhee <jonotch1@naver.com>
2023-11-21 14:31:50 +0900

    [UI] : init profile activity #8

---
+++ app/src/main/java/org/sopt/carrot/presentation/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.carrot.presentation.profile
+
+import android.os.Bundle
+import org.sopt.carrot.R
+import org.sopt.carrot.core.ui.base.BindingActivity
+import org.sopt.carrot.databinding.ActivityProfileBinding
+
+class ProfileActivity : BindingActivity<ActivityProfileBinding>(R.layout.activity_profile) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
