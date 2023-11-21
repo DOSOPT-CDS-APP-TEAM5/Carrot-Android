1c8b5fa6bb89e022d39868fe3f52928a7b383514
Jokwanhee <jonotch1@naver.com>
2023-11-21 14:30:22 +0900

    [UI] : init join meeting activity #8

--- app/src/main/java/org/sopt/carrot/presentation/exploremeeting/ExploreMeetingActivity.kt
+++ app/src/main/java/org/sopt/carrot/presentation/exploremeeting/ExploreMeetingActivity.kt
@@ -1,9 +1,11 @@
 package org.sopt.carrot.presentation.exploremeeting
 
+import android.content.Intent
 import android.os.Bundle
 import org.sopt.carrot.R
 import org.sopt.carrot.core.ui.base.BindingActivity
 import org.sopt.carrot.databinding.ActivityExploreMeetingBinding
+import org.sopt.carrot.presentation.joinmeeting.JoinMeetingActivity
 
 class ExploreMeetingActivity :
     BindingActivity<ActivityExploreMeetingBinding>(R.layout.activity_explore_meeting) {
@@ -11,7 +13,7 @@ class ExploreMeetingActivity :
         super.onCreate(savedInstanceState)
 
         binding.btnSample.setOnClickListener {
-            
+            startActivity(Intent(this, JoinMeetingActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/org/sopt/carrot/presentation/joinmeeting/JoinMeetingActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.carrot.presentation.joinmeeting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.carrot.R
+
+class JoinMeetingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_meeting)
+    }
+}
