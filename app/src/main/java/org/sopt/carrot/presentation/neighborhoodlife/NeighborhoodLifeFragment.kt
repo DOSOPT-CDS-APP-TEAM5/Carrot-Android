a6a57849ee70d5099cef2659491a47ab999c0e64
Jokwanhee <jonotch1@naver.com>
2023-11-22 22:14:28 +0900

    [ADD] : binding carouselTextAdapter

--- app/src/main/java/org/sopt/carrot/presentation/neighborhoodlife/NeighborhoodLifeFragment.kt
+++ app/src/main/java/org/sopt/carrot/presentation/neighborhoodlife/NeighborhoodLifeFragment.kt
@@ -3,15 +3,23 @@ package org.sopt.carrot.presentation.neighborhoodlife
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import org.sopt.carrot.R
 import org.sopt.carrot.core.ui.base.BindingFragment
 import org.sopt.carrot.databinding.FragmentNeighborhoodLifeBinding
+import org.sopt.carrot.presentation.neighborhoodlife.adapter.CarouselTextAdapter
+import org.sopt.carrot.presentation.neighborhoodlife.dummy.carouselTextList
 
 class NeighborhoodLifeFragment :
     BindingFragment<FragmentNeighborhoodLifeBinding>(R.layout.fragment_neighborhood_life) {
+    private lateinit var carouselTextAdapter: CarouselTextAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        carouselTextAdapter = CarouselTextAdapter()
+        binding.rcvMeeting.adapter = carouselTextAdapter
+//        binding.rcvMeeting.layoutManager = LinearLayoutManager(requireContext())
+        carouselTextAdapter.submitList(carouselTextList)
     }
 
     fun newInstance(): Fragment = NeighborhoodLifeFragment()
--- app/src/main/java/org/sopt/carrot/presentation/neighborhoodlife/dummy/CarouselText.kt
+++ app/src/main/java/org/sopt/carrot/presentation/neighborhoodlife/dummy/CarouselText.kt
@@ -2,36 +2,44 @@ package org.sopt.carrot.presentation.neighborhoodlife.dummy
 
 val carouselTextList = listOf<CarouselText>(
     CarouselText(
+        1,
         "https://s3-alpha-sig.figma.com/img/2d74/b3e4/e6df0b2ef5c55033d8062250b9625104?Expires=1701648000&Signature=nidFkLKScmpAwKV2iLramGVFzvC0hyP9q~jqMb9oAGWeNUh8Dyza63DtGOyFrP5hKwfKJYRyDl2x9hyiUFmJerTdBJdGd3GAapI6Ylrc029tUChuMf1QMa62g4alCCcDua6M0Vx9D~hTqRXOi9QVgLGh9pQzAy28dgYLeicKFg0sYWHOnJdkYpgokKasdYFMcdSm4f0Am3MhiSKMABTZ9ZRHC0XmwF4EfE9U0B0OUiY2rXjXUz1psETiADzsMSWavLQLlMNocWNu90ntHoVxKbUPvg8fICXDtkQIiEjn-sP7j2hmUsyMhpHpMARyis2L9YqzjIOkcEcFgDwhIeby-w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
         "2030 세대만 등산모임"
     ),
     CarouselText(
+        2,
         "https://s3-alpha-sig.figma.com/img/f638/9daa/dec2505360b4ae89405811e75649c949?Expires=1701648000&Signature=UdSMk6oQ5FUmNP~pV6DMzMp021J9OfEfe7rnKcMySnOeBUQozVMiXUnBTNxS-pRQVkoo5BNwS~LNNNu5Lq2~nbZI-M0ZylTmVax9g3jiET1E1B-rc91A-GJtvrsouxhOBNw0IyKbmu~iU-WyxkWJ9aRCUcvfzYCXz6ImvQcE0e3hc7AhlH-3MCayfD2I6X8Q1jKzKDL5xKZIMVoNu02SD5IcASJaH-hwJhyNGc4PzhVND~Kp23Ci7XvneNpoJhblm3UorHDSPsxPePr2r8Tdnqnlwf~O-rGZ3~34ylwHct5ei53yTV83Jk8VvNeIjc8QAQ3aUuMXnXVF9fbuqhw9aA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
         "부업, 투잡으로 모이는 우리 최강의 모임"
     ),
     CarouselText(
+        3,
         "https://s3-alpha-sig.figma.com/img/b21a/9f92/f4aa3a5940cc0ce7a3ea0a64eeaf159f?Expires=1701648000&Signature=p2EWLM0tzlPWxYrCJRUihGVinatNC624PBLbjg2EzdhTNXRb-KVx3Iamg8wPWfpnmEBf2iyfluB9AIoJespZgR88Fwep8BM70ag15D-7rkKy2uNT882YgdyS5IvdT1HflNH4~oE1nFKzJxJPeEo02BLmddfgKT7s1WKMtXaKcmCQpdtsa1sho6jsc6sPbnq0xLtKBWY06KDulmqPssHkXOD-TxkZf6rsu8I6nipQYJ3UliC5PBhYjEONSyqlER0uXqLEPslGPHkVqonnIq5o7suNXF1bxkfiv8gIKatmSy8XDJpcSwpj95O5qzIa2-UXG0DmPYKpEHj9NJW5aEUEtQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
         "팝송으로 영어 깨부시기"
     ),
     CarouselText(
+        4,
         "https://s3-alpha-sig.figma.com/img/0c30/e976/71e4084a2451a366a587b1edd38dc101?Expires=1701648000&Signature=YSj2EjGOMpc4DH0PL7cl2mNaWUoYpZKbw32xsXZUPv3x2cGJH1-a~rtuxKRdrDoYSyUwL50Tbr71WX0x6ybpRNSFYEpg3HQrXDY4J0z3ggeId4ICHcLpzYKGaGrfhfg6KKPFmNa1Ng-XPN4g1vDfC0sWEoNR3ISfiyNzCqm9k-wBkvX3OCXYvC02JeydLb7v9SYQCm47ey7IyhVcpVoZvwTLS-69MVA6WFB5EUu1rXUN0aE2mW2nq91xGqWszMkRrfYOQ4ff7g0h8u2fZNftwpXKd9vrPNXqCfRpOD5PCSKjw6Y1cQpB6MIwiP1uyz40TP0WPYeKbKbg3ZU~eoPeBg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
         "4050 친구 어때?!"
     ),
     CarouselText(
+        5,
         "https://s3-alpha-sig.figma.com/img/bf5c/c513/f00ec900d4844aa38e25e451296075a9?Expires=1701648000&Signature=QZMSl2xSWR53bot6U-5kmsfD7eh4nkSnf~pSDNSCj~uvBS07r-oW1tWF0LNLnvPNnzqYBE5bKSWXDTWxpUkQYoWNJmtKsAL6ow5dNAv09xOK4PkIc8X6z346EMLFnPcCSqXeVmRaNO0H~1i27VsKwK5axCYGTVkzXHa4uqp5~p0l~QFSEReyXg99xyD3OdKjtWHSEaWHVm5S9Q0QfzLZcjtLyUIOkJ1Rx4smvC8vidfCZdPxK0XqO1MP7YB6fxbnZ-im-Df2Hc7hBV4SaM0DWT9sbhO~ccROT-k5Lm3ieDbv2LQ9tap1a6mkg9SrrKhZEKuubdW00OqfV6S5m-noYg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
         "광교산 등산, 운동, 산책"
     ),
     CarouselText(
+        6,
         "https://s3-alpha-sig.figma.com/img/ab25/541e/302e808118aab45bd705b4673ea19531?Expires=1701648000&Signature=FDOiT~wx-NDM93TXIqg7bwQa5XYBG3ikNIl9nGrEF-uvEDC5yZATaRmghID5YA8NMGsDOCZ4sqYtR4dSZItFD7MVrZdZpyMp84w5k5IRtIhjM8vLyvQo6jtF6JHKaMo0paKjTXpA~TIF5Ex31uwkaBw23m3j02syqwkjS3yze4-bpn4hXvl9p4ueEeyaSrfAjY3loId4jF9r2VeSmWk8NBlyUJhmioCwIfyRbwW6njHgucL5lqKcYDjoXCWmT3KguRtpv4X3T1gQKyT~dACMbq~5vuJX3kpdM90nVuDTstkIY5qGg4~tYGN9~Xy7YZ7ixfw5382VgUCS1LK1ofcA7g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
         "동네 고전 문학 모여라"
     ),
     CarouselText(
+        7,
         "https://s3-alpha-sig.figma.com/img/0b09/cee6/58f586e1f3656746490c3b9a1d8059ed?Expires=1701648000&Signature=ZtAsiC2DZILNSNNhIJmqbeNKeGZ6RHGu4KsQ7R4tigX~vVz1OtPRRam67QnZXS~4ZHjkxHuVqRLVIhc~QyX0JrOutxyfHrwF2hrSKEEPfetBz0AkeY8gyzoUaRhNcjiM11W1kD3S3o7Q9~eHSpgSeFl-7EHVVqHeVtnGKGx9nmzMNdbkwfPayvvY2BxomLqbG7KLHt0qppp4KaHuf-7bxXCAOrD4Hal54L~jmQFlgK3YfAE96gLV51s5fq7Llp5BYjkeaWgecOFDWy-ymhU6xKP-8IDL2tZYSpEH7vF-nUNR84i3a8vWawIP~jjeFvkj084cQdojVvLYSTYxny1lOg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
         "동네 초딩 리코더 합주 모임 어떤데!"
     ),
 )
 
 data class CarouselText(
+    val id: Int,
     val imageUrl: String,
     val title: String
 )
